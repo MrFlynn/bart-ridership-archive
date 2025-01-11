452aca38bb30c6d8c7d624286051f37c101071fd
Nick Pleatsikas <nick@pleatsikas.me>
2025-01-11 02:51:37 -0800

    Update URL and parser for new data page.
    - Updated URL to read ridership data from since BART changed
      pages and didn't update the redirect.
    - Updated parsing logic to remove code to parse `percent_baseline`
      field since this is no longer provided. Right now the script
      will just insert a -1 into this field to keep the schema
      consistent, but I may try to find another way to calculate
      it in the future.

--- scripts/ridership.js
+++ scripts/ridership.js
@@ -1,5 +1,5 @@
 export const config = {
-    url: "https://www.bart.gov/news/articles/2023/news20230729",
+    url: "https://www.bart.gov/news/articles/2025/news20250109-1",
 }
 
 export default function ({ doc, url, absoluteURL }) {
@@ -9,20 +9,21 @@ export default function ({ doc, url, absoluteURL }) {
     rows.map(row => {
         const items = row.children()
 
-        if (items.length % 3 == 0) {
-            for (let i = 0; i < items.length; i += 3) {
+        if (items.length % 2 == 0) {
+            for (let i = 0; i < items.length; i += 2) {
                 const date = items.get(i).text().trim()
                 const ridership = Number.parseInt(items.get(i + 1).text().replace(",", ""))
-                const percent = items.get(i + 2).text().trim()
 
                 if (
                     date.match(/^([0-9]{1,2}\/){2}[0-9]{1,2}$/) !== null &&
-                    (ridership != 0 || ridership !== null || ridership !== undefined) &&
-                    (percentMatch = percent.match(/^([0-9]{1,3})%$/)) !== null 
+                    (ridership != 0 && ridership !== null && ridership !== undefined)
                 ) {
                     entries[date] = {
-                        riders: ridership, 
-                        percent_baseline: Number.parseInt(percentMatch[0]),
+                        riders: ridership,
+
+                        // BART stopped providing this information in 2025. A filler
+                        // value is provided to keep the data scheme the same.
+                        percent_baseline: -1,
                     }
                 }
             }
