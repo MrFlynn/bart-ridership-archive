734c4bdce11d0eafbf18233bfad29bb322d2450e
Nick Pleatsikas <nick@pleatsikas.me>
2025-01-25 11:18:03 -0800

    Improved functionality and correctness of scraper.
    - Simplified and improved correctness of script by mapping over
      a list of columns and taking two at a time. Thie ensures there
      are no skipped columns, unlike the last version which could skip
      columns if there were more than two in a row.
    - Added proper date validation. Dates are now in ISO8601 format.
    - Added function to calculate the percent baseline of riders using
      historical data collected by this repository.

--- scripts/ridership.js
+++ scripts/ridership.js
@@ -2,34 +2,42 @@ export const config = {
     url: "https://www.bart.gov/news/articles/2025/news20250109-1",
 }
 
+function parseDate(date) {
+    const parts = date.split("/").map(p => Number.parseInt(p))
+    return new Date(2000 + parts[2], parts[0] - 1, parts[1])
+}
+
+function calculatePercentBaseline(date, riders) {
+    // A list of target riders per day of week indexed to each
+    // day of week in it's numeric representation 
+    // (from Date.prototype.getDay).
+    const targetRidersPerDayOfWeek = [
+        102325, 382805, 409929, 408019, 401880, 404005, 152759
+    ]
+
+    return Math.trunc((riders / targetRidersPerDayOfWeek[date.getDay()]) * 100)
+}
+
 export default function ({ doc, _ }) {
-    const rows = doc.find("tr")
+    return doc.find("tr").map(
+        row => row.children()
+    ).flat().map((items) => {
+        try {
+            const date = parseDate(items.get(0).text().trim())
+            const riders = Number.parseInt(items.get(1).text().replace(",", ""))
 
-    return rows.map(row => {
-        const items = row.children()
-
-        if (items.length % 2 == 0) {
-            for (let i = 0; i < items.length; i += 2) {
-                const date = items.get(i).text().trim()
-                const ridership = Number.parseInt(items.get(i + 1).text().replace(",", ""))
-
-                if (
-                    date.match(/^([0-9]{1,2}\/){2}[0-9]{1,2}$/) !== null &&
-                    ridership > 0
-                ) {
-                    return {
-                        date: date,
-                        riders: ridership,
-                        
-                        // BART stopped providing this information in 2025. A filler
-                        // value is provided to keep the data scheme the same.
-                        percent_baseline: -1,
-                    }
+            if (date !== null && riders > 0) {
+                return {
+                    date: date.toISOString().split("T")[0],
+                    riders: riders,
+                    percent_baseline: calculatePercentBaseline(date, riders),
                 }
             }
+        } catch (_) {
+            return null
         }
     }).reduce((accumulator, entry) => {
-        if (entry !== null) {
+        if (entry !== null && entry !== undefined) {
             accumulator[entry.date] = {
                 riders: entry.riders,
                 percent_baseline: entry.percent_baseline,
